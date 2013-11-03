68a62c46e223275633ea47f8e5d56e26f246766d
cbora <cbora@d8a59d42-d5e9-4b05-92fb-be7e80f78f5f>
2013-11-03 04:42:43 +0000

    fixed bug about searching files of different string length, with 2 section of numbers included

--- Framework/fileList2.pl
+++ Framework/fileList2.pl
@@ -17,35 +17,61 @@ sub FindFiles {
     my @findResults = split /\n/, $oneLineFindResults;
     my @fileList;
 
-
 #check if basename of from and to has the same length                                                                                                                                      
     $fromlength= length $from;
     $tolength= length $to;
     if($fromlength != $tolength){
+	
+	#if string is L0.runXXX
+	if((index($from , 'L0.run') != -1) || (index($from, 'L1.run') != -1) || (index($from, 'L2.run') != -1)){
        #if the lenght of the from and to is not equal                                   
-        my ($fromstr)=$from =~ /(\d+)/;
-        my ($tostr)=$to =~ /(\d+)/;
+        my $newFrom= substr $from, index($from, 'un');
+        my ($fromstr)=$newFrom =~ /(\d+)/;
+        my $newTo = substr $to, index($to, 'un');
+        my ($tostr)=$newTo =~ /(\d+)/;
         $fromstr="0".$fromstr;
-	my ($tol) =length $tostr;
+        my ($tol) =length $tostr;
 
-	for my $full (@findResults) {
-	    $basename = basename($full);
+        for my $full (@findResults) {
+            $basename = basename($full);
+             my $newBasename = substr $basename, index($basename, 'un');
+             my ($basenamestr)= $newBasename =~ /(\d+)/;
+             my $basenamelength = length $basenamestr;
+            if($basenamelength != $tol){
+                $basenamestr = "0".$basenamestr;
+            }
+            if((!defined($fromstr) or $basenamestr ge $fromstr) and (!defined($tostr) or $basenamestr le $tostr)) {
+                push (@fileList, $full);
+            }
+        }
+
+	} else{
+	    my ($fromstr)=$from =~ /(\d+)/;
+	    my ($tostr)=$to =~ /(\d+)/;
+	    $fromstr="0".$fromstr;
+	    my ($tol) =length $tostr;
+
+	    for my $full (@findResults) {
+		$basename = basename($full);
              my ($basenamestr)= $basename =~ /(\d+)/;
              my $basenamelength = length $basenamestr;
-	    if($basenamelength != $tol){
-		$basenamestr = "0".$basenamestr;
+		if($basenamelength != $tol){
+		    $basenamestr = "0".$basenamestr;
+		}
+		if((!defined($fromstr) or $basenamestr ge $fromstr) and (!defined($tostr) or $basenamestr le $tostr)) {
+		    push (@fileList, $full);
+		}
 	    }
-	    if((!defined($fromstr) or $basenamestr ge $fromstr) and (!defined($tostr) or $basenamestr le $tostr)) {
-		push (@fileList, $full);
-	    }
+
 	}
+	    
     }else{
-	for my $full (@findResults) {
-	    $basename = basename($full);
-	    if ((!defined($from) or $basename ge $from) and (!defined($to) or $basename le $to)) {
-		push(@fileList, $full);
-	    }
-	}
+for my $full (@findResults) {
+            $basename = basename($full);
+            if ((!defined($from) or $basename ge $from) and (!defined($to) or $basename le $to)) {
+                push(@fileList, $full);
+            }
+        }
     }
     @fileList = sort(@fileList);
     my $tot = @findResults;
@@ -57,14 +83,14 @@ sub FindFiles {
 
 # Prints file list to a file
 sub PrintFileListToFile {
-	my @list = @{@_[0]};
-	my $fileName = ${@_[1]};
-	open(FILE, ">$fileName") or die "Cannot open file $fileName : $!\n";
-	for my $line (@list) {
-		#print "$line\n";
-		print FILE "$line\n";
-	}
-	close(FILE);
+        my @list = @{@_[0]};
+        my $fileName = ${@_[1]};
+        open(FILE, ">$fileName") or die "Cannot open file $fileName : $!\n";
+        for my $line (@list) {
+                #print "$line\n";
+                print FILE "$line\n";
+        }
+        close(FILE);
 }
 
 #($ref1, $ref2) = FindFiles("/data/exp/ARA/2012/filtered/L0/", "*.root", "run010944.root", "run010955.root");
